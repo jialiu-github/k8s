61be82ad1b01814013e2a7314f09ec882e99f5e2
jialiu <jialiu@thoughtworks.com>
2019-03-15 16:28:45 +0800

    [liujia] finish 002 ac01

---
+++ 002/appForK8S/Controllers/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace appForK8S.Controllers
+{
+    [Route("api/[controller]")]
+    public class NameController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public string Get()
+        {
+            return "liu Jia";
+        }
+
+    }
+}
