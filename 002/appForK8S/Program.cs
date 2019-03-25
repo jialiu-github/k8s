5139c9adf45d48aa4856d413078c98ad15fd67b8
jialiu <jialiu@thoughtworks.com>
2019-03-25 08:58:10 +0800

    [liujia] 003 finish the homework

--- 002/appForK8S/Program.cs
+++ 002/appForK8S/Program.cs
@@ -20,6 +20,7 @@ namespace appForK8S
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseUrls("http://0.0.0.0:12345/")
                 .Build();
     }
 }
